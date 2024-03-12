ce6586732e5679fddf9d2c044db120e8216cb16c
Amal Nisha <amalnisha0705@gmail.com>
2024-03-12 02:55:27 -0700

    Added JS

---
+++ product.js
@@ -0,0 +1,31 @@
+
+    var menuicon = document.getElementById("menuicon");
+    var sidenav = document.getElementById("sidenav");
+    var closenav = document.getElementById("closenav");
+
+    menuicon.addEventListener("click", function () {
+        sidenav.style.right = 0
+    });
+
+    closenav.addEventListener("click", function () {
+        sidenav.style.right = "-50%"
+    });
+    var productcontainer = document.getElementById("product-container");
+var search = document.getElementById("search");
+var productlist = productcontainer.querySelectorAll("div");
+
+search.addEventListener("keyup", function (event) {
+    var enteredValue = event.target.value.toUpperCase();
+    for (var count = 0; count < productlist.length; count = count + 1) {
+        var productname = productlist[count].querySelector("h1").textContent;
+        if (productname.toUpperCase().indexOf(enteredValue) < 0) {
+            productlist[count].style.display = "none";
+        } else {
+            productlist[count].style.display = "block";
+        }
+    }
+});
+
+     
+
+    
